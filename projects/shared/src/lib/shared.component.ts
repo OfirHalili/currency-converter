00c31159932293a5963fe3f8ab6c4a4f3d3dc1fb
ofirhalili <ofirhalili@gmail.com>
2025-05-05 11:39:44 +0300

    generate shared library

---
+++ projects/shared/src/lib/shared.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cc-shared',
+  imports: [],
+  template: `
+    <p>
+      shared works!
+    </p>
+  `,
+  styles: ``
+})
+export class SharedComponent {
+
+}

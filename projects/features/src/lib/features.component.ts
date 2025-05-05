fee71a990a0d9513492e317ba213e6e4ba4491f7
ofirhalili <ofirhalili@gmail.com>
2025-05-05 11:34:47 +0300

    generate features library

---
+++ projects/features/src/lib/features.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cc-features',
+  imports: [],
+  template: `
+    <p>
+      features works!
+    </p>
+  `,
+  styles: ``
+})
+export class FeaturesComponent {
+
+}
